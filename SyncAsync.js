506e256523dfdbcd6eef011c26b591727ebf8e38
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-09-06 19:49:05 +0600

    callback hell

---
+++ SyncAsync.js
@@ -0,0 +1,11 @@
+// start
+
+console.log("welcome to ACCCC");
+
+//  Ultimately even if we update or receive product data from databases such as prices , id & many more if we do it as setimeout
+// it will be complete callback hell
+setTimeout(() => {
+  const user = {id: 1};
+},1000)
+
+console.log("hope you are learnig well")
