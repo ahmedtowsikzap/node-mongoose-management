a1d69b2a2086dc2167a1de0c83efc729bc831427
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-09-08 18:12:38 +0600

    multiple promise handleling

--- Promises.js
+++ Promises.js
@@ -5,17 +5,32 @@
 //rejected
 
  const myPromise = new Promise((resolve, reject) => {
-    const user = null;
+    const user = true;
     if(!user){
  reject("users is not present in the database!")
     } else {
         setTimeout(() =>
         {
-         resolve("got the data")
+         resolve({name: "john"})
         }, 1000);
     }
    
 
  });
 
- 
+const userId = [1,2,4,56,7,8];
+let userdata = []
+for(let i = 0; i< userId.length; i++){
+    const user = userId[i]
+   userdata.push(myPromise) 
+}
+Promise.all(userdata).then(res => {
+    console.log(res)
+})
+// console.log(userdata)
+
+ myPromise
+ .then(res => console.log(res))
+ .catch(err => console.log(err));
+
+ console.log("done")
